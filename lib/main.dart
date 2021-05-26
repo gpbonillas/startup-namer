4a9438f2f1ad28f86a038f772f8381dfc5a73d8e
Pato Bonilla <gpbonillas@gmail.com>
2021-05-26 12:32:00 -0500

    Change the theme for the UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.lightGreen[800],
+      ),
       home: RandomWords(),
     );
   }
